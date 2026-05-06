317c966a0e19cb893acde25b3e23abe70526d4ba
Even Solomon <Evensenay04@gmail.com>
2026-05-06 20:56:22 +0200

    Hattan Palace

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:0507@localhost:5432/hotell_db"
+DATABASE_URL = "postgresql://hatan_palace_db_envb_user:CrInGP3c2bGSvk881a5MChVsuEn87UWs@dpg-d7toedbtqb8s73d5pplg-a/hatan_palace_db_envb"
 
 
 engine = create_engine(DATABASE_URL)
